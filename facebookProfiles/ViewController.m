ed94f11fecd86b4eb8066b173ee1e73d6079079f
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-09-06 17:55:43 -0400

    display posts

--- facebookProfiles/ViewController.m
+++ facebookProfiles/ViewController.m
@@ -9,11 +9,13 @@
 #import "ViewController.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+@import WebKit;
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSDictionary *data;
+@property (nonatomic, strong) NSArray *posts;
 
 @end
     
@@ -22,7 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
-    loginButton.readPermissions = @[@"email", @"public_profile"];
+    loginButton.readPermissions = @[@"email", @"public_profile", @"user_posts"];
     // Optional: Place the button in the center of your view.
     loginButton.center = CGPointMake(self.view.frame.size.width / 2, 60);
     [self.view addSubview:loginButton];
@@ -30,10 +32,14 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     if ([FBSDKAccessToken currentAccessToken]) {
-        [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:nil]
+        [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me/posts" parameters:nil]
          startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
              if (!error) {
+                 NSLog(@"%@", result);
+                 
+                 
                  self.data = result;
+                 self.posts = [self.data objectForKey:@"data"];
                  self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, self.view.frame.size.height - 100) style:UITableViewStyleGrouped];
                  self.tableView.dataSource = self;
                  self.tableView.delegate = self;
@@ -48,13 +54,13 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return self.posts.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
     if (cell) {
-        cell.textLabel.text = [self.data objectForKey:@"name"];
+        cell.textLabel.text = [[self.posts objectAtIndex:indexPath.row] objectForKey:@"message"];
     }
     return cell;
 }
